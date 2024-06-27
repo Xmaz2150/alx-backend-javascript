71036563ee1732c48c8cbb9910cf8564d453de98
tAf <muzirat0@gmail.com>
2024-06-27 18:48:49 +0000

    finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
+  return promise.then(() => {
     return { status: 200, body: 'success' };
   }).catch(() => {
-    console.log('Got a response from the API');
     return Error('');
+  }).finally(() => {
+    console.log('Got a response from the API');
   });
 }
