eb8f559ec665119ac8792413cbea535eefcc7b01
tAf <muzirat0@gmail.com>
2024-06-27 19:54:56 +0000

    consuming successful promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import {uploadPhoto, createUser} from './utils.js';
+
+export default function handleProfileSignup() {
+  let description = "";
+
+  return Promise.all([uploadPhoto(), createUser()])
+  .then((user) => {
+    description += user[0]['body'] + " ";
+    description += user[1]['firstName']
+    + " " +
+    user[1]['lastName'];
+  })
+  .catch(() => { console.log("Signup system offline") })
+  .finally(() => { console.log(description) });
+}
