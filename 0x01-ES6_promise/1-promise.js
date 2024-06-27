6229f39d4fd998fa23c5ae8c813bbc946c3abc9a
tAf <muzirat0@gmail.com>
2024-06-27 12:46:42 +0000

    lint and error reject

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,13 @@
 export default function getFullResponseFromAP(success) {
   return new Promise((resolve, reject) => {
-    if(success) {
+    if (success) {
       const response = {
-        'status': 200,
-        'body': 'Success',
+        status: 200,
+        body: 'Success',
       };
       resolve(response);
-    }
-    else {
-      reject("The fake API is not working currently");
+    } else {
+      reject(Error("The fake API is not working currently"));
     }
   });
 }
