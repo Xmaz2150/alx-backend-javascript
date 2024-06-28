604ac494e068c424fa1932df4e23d1ba7cc716cd
tAf <muzirat0@gmail.com>
2024-06-28 11:47:51 +0000

    dust settles

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,5 +6,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ]);
+  ])
+  .then((results) => {
+    return results.map(
+      (result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+      })
+    );
+  });
 }
