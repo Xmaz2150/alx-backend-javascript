9bd66406395b7458cd548e3eb22457bb70bc4ed3
tAf <muzirat0@gmail.com>
2024-06-28 13:50:01 +0000

    stricr inherit

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+class Building {
+
+  constructor(sqft) {
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) {
+      throw Error("Class extending Building must override evacuationWarningMessage");
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
