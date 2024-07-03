97eecdea8e93f4c904808963202dddb69c46144c
tAf <muzirat0@gmail.com>
2024-07-03 21:00:45 +0200

    print teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,10 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+//task 3
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => `${firstName[0]}. ${lastName}`;
