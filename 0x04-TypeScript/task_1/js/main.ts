62e8624243dbd43f518c9994e28fe8d9197537f8
tAf <muzirat0@gmail.com>
2024-07-03 21:02:05 +0200

    class by interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,31 @@ interface printTeacherFunction {
 }
 
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => `${firstName[0]}. ${lastName}`;
+
+//task 4
+interface Student {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentConstructor {
+  (firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements Student{
+  firstName: string;
+  lastName: string;
+ 
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName =  lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
