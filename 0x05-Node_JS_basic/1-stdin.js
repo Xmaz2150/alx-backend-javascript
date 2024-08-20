849aa472a7ce89053ca88b8ba743f11059737d59
tAf <muzirat0@gmail.com>
2024-08-20 14:18:52 +0200

    1. Using Process stdin update: handle pipe and interactive mode, removed log helper

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,14 @@
-const displayMessage = require('./0-console');
-
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  displayMessage(`Your name is: ${data}`);
-  process.exit();
+  process.stdout.write(`Your name is: ${data}`);
+
+  if (!process.stdin.isTTY) {
+    process.stdin.emit('end');
+  }
+});
+
+process.stdin.on('end', () => {
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
 });
