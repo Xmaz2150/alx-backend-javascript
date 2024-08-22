fb65c86e92f3981974f576a4e3e745f8600c0557
tAf <muzirat0@gmail.com>
2024-08-22 22:42:04 +0200

    new test cases

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -39,9 +39,22 @@ describe('Cart Page', function() {
       expect(body).to.equal('Payment methods for cart 12');
     });
   });
+
   it('returns status code 404', function() {
     request.get('http://localhost:7865/cart/notNumber', function(error, response, body) {
       expect(response.statusCode).to.equal(404);
+    });
+  });
+
+  it('returns correnct result on wrong input', function() {
+    request.get('http://localhost:7865/cart/notNumber', function(error, response, body) {
+      expect(body).to.contain('Cannot GET /cart/notNumber');
+    });
+  });
+
+  it('returns the right content type', function() {
+    request.get('http://localhost:7865/cart/12', function(error, response, body) {
+      expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
     });
   });
 });
