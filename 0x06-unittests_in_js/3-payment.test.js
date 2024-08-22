63878ddd8f79a33802ad3562bcab185b2a279eb6
tAf <muzirat0@gmail.com>
2024-08-22 16:31:24 +0200

    imports and calling fix, new test cases

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function() {
   let spy;
@@ -13,7 +14,19 @@ describe('sendPaymentRequestToApi', function() {
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
   });
 
+  it('validates sendPaymentRequestToApi is called once', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('validates sendPaymentRequestToApi logs correct message', function() {
+    sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 120')).to.be.true;
+    console.log.restore();
+  });
+
   afterEach(() => {
-    sinon.restore();
+    spy.restore();
   });
 });
