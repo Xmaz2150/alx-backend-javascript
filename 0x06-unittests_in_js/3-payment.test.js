109511dde666c9e7f368bd104000c8144a558c89
tAf <muzirat0@gmail.com>
2024-08-22 15:01:24 +0200

    3. Spies update: before and after each

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,11 +3,16 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function() {
-  it('validates the usage of the Utils.calculateNumber function', function() {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(Utils, 'calculateNumber');
+  });
 
+  afterEach(() => {
+    spy.restore();
+  });
+  it('validates the usage of the Utils.calculateNumber function', function() {
     Utils.calculateNumber('SUM', 100, 20);
     assert(spy.calledWith('SUM', 100, 20));
-    spy.restore();
   });
 });
