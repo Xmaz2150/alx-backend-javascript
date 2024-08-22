45dceddabd60d52871748727b992b0b54259c508
tAf <muzirat0@gmail.com>
2024-08-22 22:31:31 +0200

    new test case

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -22,22 +22,26 @@ describe('Index Page', function() {
 
   it('returns status code 404', function() {
     request.get('http://localhost:7865/any', function(error, response, body) {
-    expect(response.statusCode).to.equal(404);
+      expect(response.statusCode).to.equal(404);
     });
   });
 
 });
 
 describe('Cart Page', function() {
-  it('returns status code 404', function() {
-    request.get('http://localhost:7865/cart/notNumber', function(error, response, body) {
-    expect(response.statusCode).to.equal(404);
-    });
-  });
-
   it('returns status code 200 with int', function() {
     request.get('http://localhost:7865/cart/12', function(error, response, body) {
-    expect(response.statusCode).to.equal(200);
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+  it('returns correct result', function() {
+    request.get('http://localhost:7865/cart/12', function(error, response, body) {
+      expect(body).to.equal('Payment methods for cart 12');
+    });
+  });
+  it('returns status code 404', function() {
+    request.get('http://localhost:7865/cart/notNumber', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
     });
   });
 });
