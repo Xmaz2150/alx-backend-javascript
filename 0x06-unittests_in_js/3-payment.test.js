96a08d24b1aef7fcd2dc9c20158ac1c3e079f6fe
tAf <muzirat0@gmail.com>
2024-08-22 15:20:50 +0200

    assert to chai

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
 
@@ -13,6 +13,6 @@ describe('sendPaymentRequestToApi', function() {
   });
   it('validates the usage of the Utils.calculateNumber function', function() {
     Utils.calculateNumber('SUM', 100, 20);
-    assert(spy.calledWith('SUM', 100, 20));
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
   });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
@@ -19,7 +19,7 @@ describe('sendPaymentRequestToApi', function() {
 
   it('stubs Utils.calculateNumber to always return 10', function() {
     sendPaymentRequestToApi(100, 20);
-    assert(calculateNumberStub.calledWith('SUM', 100, 20));
-    assert(consoleSpy.calledWith('The total is: 10'));
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
   });
 });
