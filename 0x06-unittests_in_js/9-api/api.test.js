56ebfeb7c48c2e9114beaac2d231af48102360a3
tAf <muzirat0@gmail.com>
2024-08-22 22:21:23 +0200

    9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index Page', function() {
+  it('returns status code 200', function() {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+
+  it('returns correct result', function() {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+
+  it('returns the right content type', function() {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
+    });
+  });
+
+  it('returns status code 404', function() {
+    request.get('http://localhost:7865/any', function(error, response, body) {
+    expect(response.statusCode).to.equal(404);
+    });
+  });
+
+});
+
+describe('Cart Page', function() {
+  it('returns status code 404', function() {
+    request.get('http://localhost:7865/cart/notNumber', function(error, response, body) {
+    expect(response.statusCode).to.equal(404);
+    });
+  });
+
+  it('returns status code 200 with int', function() {
+    request.get('http://localhost:7865/cart/12', function(error, response, body) {
+    expect(response.statusCode).to.equal(200);
+    });
+  });
+});
