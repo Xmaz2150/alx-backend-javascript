df2b7d81263f9049a58a6d1e5d9223cc1af17023
tAf <muzirat0@gmail.com>
2024-08-22 15:31:43 +0200

    no spy, new test cases

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,23 +3,33 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', function() {
+
+describe('sendPaymentRequestToApi', function () {
   let calculateNumberStub;
-  let consoleSpy;
 
   beforeEach(() => {
+    /* stubs Utils.calculateNumber to always return 10 */
     calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    consoleSpy = sinon.spy(console, 'log');
   });
 
-  it('stubs Utils.calculateNumber to always return 10', function() {
+  it('called with the two arguments', () => {
     sendPaymentRequestToApi(100, 20);
     expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+  });
+
+  it('called once with arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnce).to.be.true;
+  });
+
+  it('console log with the correct sum parameter', () => {
+    sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 10')).to.be.true;
+    console.log.restore();
   });
 
   afterEach(() => {
-    calculateNumberStub.restore();
-    consoleSpy.restore();
+    sinon.restore();
   });
 });
