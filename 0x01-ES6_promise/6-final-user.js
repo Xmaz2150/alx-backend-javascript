448c5f2e89c29954a65b088ea7ff9e3bd6b3b0bc
tAf <muzirat0@gmail.com>
2024-10-15 16:44:29 +0200

    clean promise status & value for rejected promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,21 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ])
-  .then((results) => {
-    return results.map(
-      (result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      })
-    );
-  });
+    .then((results) => {
+      results.map(
+        (result) => {
+          if (result.status !== 'fulfilled') {
+            result.value = result.reason.toString().split('\n')[0];
+            delete result.reason;
+          }
+        },
+      );
+     return results;
+    });
 }
