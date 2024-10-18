088ad1cad4ce5988b48dd78fdde27a615d25f1eb
tAf <muzirat0@gmail.com>
2024-10-18 16:48:07 +0200

    style

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,11 +1,11 @@
+/* eslint no-underscore-dangle: ['error', { 'allow': ['_name', '_code'] }] */
 class AirPort {
-
   constructor(name, code) {
-    if (typeof name !== "string") {
-      throw TypeError("Name must be of type string");
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be of type string');
     }
-    if (typeof code !== "string") {
-      throw TypeError("Code must be of type string");
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be of type string');
     }
     this._name = name;
     this._code = code;
