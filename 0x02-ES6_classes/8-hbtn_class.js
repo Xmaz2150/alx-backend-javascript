59f129930e6c30e2cdaff22542db78fbbe5b9266
tAf <muzirat0@gmail.com>
2024-10-19 12:49:30 +0200

    style

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,15 +1,16 @@
-class HolbertonClass {
+/* eslint no-underscore-dangle: ["error", { "allow": ["_size", "_location"] }] */
 
+class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
   [Symbol.toPrimitive](atr) {
-    if (atr === "number") {
-      return "" + this._size;
+    if (atr === 'number') {
+      return this._size;
     }
-    return "" + this._location;
+    return this._location;
   }
 }
 
